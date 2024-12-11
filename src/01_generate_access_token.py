dd1dbb3159668898ca89ee7705962d55ff559876
morgan wang <morgan.xu.wang@gmail.com>
2024-12-11 11:51:53 +1000

    Add timestamp to the config file.

--- src/01_generate_access_token.py
+++ src/01_generate_access_token.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -20,32 +22,23 @@ API_Key = config['API_Key']
 API_Key_Secret = config['API_Key_Secret']
 
 
-print(f"API Key: {API_Key}")
-print(f"API Key Secret: {API_Key_Secret}")
-
 ####### Get Access Token ###########
-# token_url = "https://oauth-m2m.auth.ap-southeast-2.amazoncognito.com/oauth2/token"
 token_url = "https://auth-m2m.megaport.com/oauth2/token"
-requests.packages.urllib3.disable_warnings()
 
 headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 payload = {'grant_type': 'client_credentials'}
 
-# response = requests.post(token_url, auth=HTTPBasicAuth(API_Key, API_Key_Secret), headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'grant_type': 'client_credentials'})
-# response = requests.post(token_url, auth=HTTPBasicAuth(API_Key, API_Key_Secret), headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'grant_type': 'client_credentials'}, verify=False)
 response = requests.request("POST", token_url, auth=HTTPBasicAuth(API_Key, API_Key_Secret), headers=headers, data=payload)
-
-
-print("megaport_staging_GenAccessToken(): HtmlResponse.status_code = ", response.status_code)
 
 
 if response.status_code == 200:
 
    access_token = response.json()['access_token']
-   print(f"Get JWT token: {access_token}")
 
    # Add or update a value
    config['access_token'] = access_token
+   timestamp_now = datetime.datetime.now()
+   config['token_timestamp'] = timestamp_now.strftime("%Y-%m-%d  %H-%M-%S")
 
    # Write the updated config back to the file
    write_config(config)
