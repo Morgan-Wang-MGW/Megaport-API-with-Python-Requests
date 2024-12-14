caae44eb414b1313db585078e7a37d53d770bc47
morgan wang <morgan.xu.wang@gmail.com>
2024-12-15 01:03:34 +1100

    Get Billing Markets.py Set Billing Market.py minor updates

--- src/02_Account_Setup/Get Billing Markets.py
+++ src/02_Account_Setup/Get Billing Markets.py
@@ -1,5 +1,4 @@
 import json
-
 import requests
 
 # Function to read the config file
--- src/02_Account_Setup/Set Billing Market.py
+++ src/02_Account_Setup/Set Billing Market.py
@@ -1,22 +1,10 @@
 import json
-
 import requests
 
 # Function to read the config file
 def read_config():
     with open('../../config/config.json', 'r') as config_file:
         return json.load(config_file)
-
-# Function to write to the billing markets file
-def write_billing_market(billing_market):
-    try:
-        with open('billing_market.json', 'w') as billing_market_file:
-            json.dump(billing_market, billing_market_file, indent=4)
-        print("Successfully wrote billing market to billing_market.json")
-    except IOError as e:
-        print(f"An error occurred while writing to the file: {e}")
-    except TypeError as e:
-        print(f"The data is not JSON serializable: {e}")
 
 # Read the existing config
 config = read_config()
@@ -51,16 +39,10 @@ response = requests.request("POST", request_url, headers=bearer_token_headers, d
 
 if response.status_code == 200:
 
-   #Read json from response
-   # billing_market = response.json()
-
-   # Write the data to json file
-   # write_billing_market(billing_market)
-   print(f'SUCCESS to get Billing Markets, status code: {response.status_code}')
-   print(response.text)
+   print(f'Successfully set Billing Market')
 
 else:
-   print(f'Failed to get Billing Markets, status code: {response.status_code}')
+   print(f'Failed to set Billing Markets, status code: {response.status_code}')
 
 
 
