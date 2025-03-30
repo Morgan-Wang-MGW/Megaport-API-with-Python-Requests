f3e1281a314e1f2007441a73124bcbfcfc10debe
morgan wang <morgan.xu.wang@gmail.com>
2025-03-31 09:55:54 +1100

    new files for MVE Image

---
+++ src/08_MVE/MVE_Image_Details.py
@@ -0,0 +1,47 @@
+import json
+import requests
+
+# Function to read the config file
+def read_config():
+    with open('../../config/config.json', 'r') as config_file:
+        return json.load(config_file)
+
+# Function to write to the location dictionary file
+def write_dictionary(image_dictionary):
+    try:
+        with open('image_dictionary.json', 'w') as image_file:
+            json.dump(image_dictionary, image_file, indent=4)
+        print("Successfully wrote image to image_dictionary.json")
+    except IOError as e:
+        print(f"An error occurred while writing to the file: {e}")
+    except TypeError as e:
+        print(f"The data is not JSON serializable: {e}")
+
+# Read the existing config
+config = read_config()
+
+access_token = config['access_token']
+
+bearer_token_headers = {
+   'Content-Type': 'application/json',
+   'Authorization': f'Bearer {access_token}'
+}
+
+####### Get Image ###########
+image_url = f"https://api.megaport.com/v3/product/mve/images"
+
+response = requests.get(image_url, headers=bearer_token_headers)
+
+if response.status_code == 200:
+
+    print('Successfully get image dictionary')
+    image_dictionary = response.json()
+    # Write the location data to json file
+    write_dictionary(image_dictionary)
+
+else:
+   print(f'Failed to get image dictionary, status code: {response.status_code}')
+
+
+
+
